705249e3e409165135bd687d220aa5b4e1c98617
ozodb <ozodbekkarimov100@gmail.com>
2022-12-08 16:55:35 +0100

    Initial commits

---
+++ app/src/main/java/pl/ozodbek/telegramui/MainActivity.java
@@ -0,0 +1,18 @@
+package pl.ozodbek.telegramui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppCompatActivity binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+//Mekramen library
